736f6e5762f4dc1f9300288c52c4e456698666aa
Hiroya-W <hyuyukun@gmail.com>
2022-07-02 01:01:57 +0900

    WIP: Add all files for g++

---
+++ g++/HardwareDebug/src/gcc_rx_project.d
@@ -0,0 +1,4 @@
+src/gcc_rx_project.o: ../src/gcc_rx_project.cpp \
+ C:/Users/HIRO/e2_studio/workspace2/gcc_rx_project/generate/iodefine.h
+
+C:/Users/HIRO/e2_studio/workspace2/gcc_rx_project/generate/iodefine.h:
