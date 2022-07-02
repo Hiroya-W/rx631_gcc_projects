35bf7981c62465b7c418b9e3572f5e96e34b51c4
Hiroya-W <hyuyukun@gmail.com>
2022-07-02 15:29:59 +0900

    :wastebasket: Remove unnecessary files in g++

--- g++/HardwareDebug/src/gcc_rx_project.d
+++
@@ -1,4 +0,0 @@
-src/gcc_rx_project.o: ../src/gcc_rx_project.cpp \
- C:/Users/HIRO/e2_studio/workspace2/gcc_rx_project/generate/iodefine.h
-
-C:/Users/HIRO/e2_studio/workspace2/gcc_rx_project/generate/iodefine.h:
